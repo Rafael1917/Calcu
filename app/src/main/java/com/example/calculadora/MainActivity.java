c033ca46b264b921a1d571673f12efc62c2e7ad7
Rafael1917 <19170061@uttcampus.edu.mx>
2020-10-26 14:26:46 -0600

    Codigo semi final (creo)

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -10,12 +10,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button //Numeros
-            btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0,btnp,
+
+    Button
+            //Numeros
+            btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnp,
+            //Operaciones
+            btnsum, btnres, btnmul, btndiv,
+            btnig,
             //Funciones
-            btnclr,btnborr,btnce, btnmister,btnmac,btnmmas,btnmmenos,btnbandams,
-       //Operaciones
-          btnsum, btnres, btnmul, btndiv, btnig;
+            btnclr,btnborr,btnce;
     TextView label;
     String res, n1, n2, ide;
     Double doble;
@@ -24,10 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //-------------------------INICIA OPERACIONES------------------------------------
-
+        //-------------------------INICIA OPERACIONES-------------------------------------
         btnsum = findViewById(R.id.mas);
         btnsum.setOnClickListener(funciones);
 
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnmul = findViewById(R.id.por);
         btnmul.setOnClickListener(funciones);
-        //-------------------------TERMINA OPERACIONES-------------------------------------
+//-------------------------TERMINA OPERACIONES-------------------------------------
 
         //-------------------------INICIA FUNCIONES-------------------------------------
         btnclr = findViewById(R.id.clear);
@@ -48,53 +48,86 @@ public class MainActivity extends AppCompatActivity {
         btnborr = findViewById(R.id.borr);
         btnborr.setOnClickListener(funciones);
 
-        btnmac = findViewById(R.id.mac);
-        btnmac.setOnClickListener(funciones);
-
-        btnmister = findViewById(R.id.mister);
-        btnmister.setOnClickListener(funciones);
-
-        btnmmas = findViewById(R.id.mmas);
-        btnmmas.setOnClickListener(funciones);
-
-        btnmmenos = findViewById(R.id.mmenos);
-        btnmmenos.setOnClickListener(funciones);
-
-        btnbandams = findViewById(R.id.bandams);
-        btnbandams.setOnClickListener(funciones);
-
         btnce = findViewById(R.id.ce);
         btnce.setOnClickListener(funciones);
 
         btnig = findViewById(R.id.igual);
-        btnig.setOnClickListener(peraciones);
+        btnig.setOnClickListener(Operaciones);
         //-------------------------TERMINA FUNCIONES-------------------------------------
 
         //--------------------------INICIA NUMEROS----------------------------------------
         btn1 = findViewById(R.id.uno);
+        btn1.setOnClickListener(nums);
 
         btn2 = findViewById(R.id.dos);
+        btn2.setOnClickListener(nums);
 
         btn3 = findViewById(R.id.tres);
+        btn3.setOnClickListener(nums);
 
         btn4 = findViewById(R.id.cuatro);
+        btn4.setOnClickListener(nums);
 
         btn5 = findViewById(R.id.cinco);
+        btn5.setOnClickListener(nums);
 
         btn6 = findViewById(R.id.seis);
+        btn6.setOnClickListener(nums);
 
         btn7 = findViewById(R.id.siete);
+        btn7.setOnClickListener(nums);
 
         btn8 = findViewById(R.id.ocho);
+        btn8.setOnClickListener(nums);
 
         btn9 = findViewById(R.id.nueve);
+        btn9.setOnClickListener(nums);
 
         btn0 = findViewById(R.id.cero);
+        btn0.setOnClickListener(nums);
 
         btnp = findViewById(R.id.punto);
+        btnp.setOnClickListener(nums);
         //--------------------------TERMINA NUMEROS----------------------------------------
-
+        label = (TextView) findViewById(R.id.resultado);
     }
+
+
+    //==============================================================================================
+    View.OnClickListener Operaciones = new View.OnClickListener() {
+        @Override
+        public void onClick(View vh) {
+            Button operacion = (Button) vh;
+            String texto = operacion.getText().toString();
+            n2 = label.getText().toString();
+            doble = Double.parseDouble(n1) + Double.parseDouble(n2);
+
+            switch (ide) {
+                //Entre
+                case "/":
+                    doble = Double.parseDouble(n1) / Double.parseDouble(n2);
+                    label.setText(doble.toString());
+                    break;
+                //Por
+                case "*":
+                    doble = Double.parseDouble(n1) * Double.parseDouble(n2);
+                    label.setText(doble.toString());
+                    break;
+                //Menos
+                case "-":
+                    doble = Double.parseDouble(n1) - Double.parseDouble(n2);
+                    label.setText(doble.toString());
+                    break;
+                //Mas
+                case "+":
+                    doble = Double.parseDouble(n1) + Double.parseDouble(n2);
+                    label.setText(doble.toString());
+                    break;
+
+            };
+        }
+    };
+    //==============================================================================================
     View.OnClickListener funciones = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -107,35 +140,19 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.mas:
                     label.setText("");
                     break;
-                case R.id.por:
-                    label.setText("");
-                    break;
-                case R.id.clear:
-                    label.setText("");
-                    break;
-                    //Investigar como implementar estas funciones
-                case R.id.mac:
-                    label.setText("");
-                    break;
-                case R.id.mister:
-                    label.setText("");
-                    break;
-                case R.id.mmas:
-                    label.setText("");
-                    break;
                 case R.id.menos:
                     label.setText("");
                     break;
                 case R.id.entre:
                     label.setText("");
                     break;
-                case R.id.mmenos:
+                case R.id.por:
                     label.setText("");
                     break;
-                case R.id.bandams:
+                case R.id.clear:
                     label.setText("");
                     break;
-                    //Hasta Aqui
+                //Checar bien como hacer para borrar solo 1 numero
                 case R.id.borr:
                     label.setText("");
                     break;
@@ -145,30 +162,20 @@ public class MainActivity extends AppCompatActivity {
             };
         }
     };
-
-    View.OnClickListener peraciones = new View.OnClickListener() {
+    //==============================================================================================
+    View.OnClickListener nums = new View.OnClickListener() {
         @Override
-        public void onClick(View vh) {
-            Button operacion = (Button) vh;
-            String texto = operacion.getText().toString();
-            n2 = label.getText().toString();
-            doble = Double.parseDouble(n1) + Double.parseDouble(n2);
-
-            switch (ide) {
-                case "/": doble = Double.parseDouble(n1) / Double.parseDouble(n2);
-                    label.setText(doble.toString());
-                    break;
-                case "*": doble = Double.parseDouble(n1) * Double.parseDouble(n2);
-                    label.setText(doble.toString());
-                    break;
-                case "-": doble = Double.parseDouble(n1) - Double.parseDouble(n2);
-                    label.setText(doble.toString());
-                    break;
-                case "+": doble = Double.parseDouble(n1) + Double.parseDouble(n2);
-                    label.setText(doble.toString());
-                    break;
-
-            };
+        public void onClick(View v) {
+            Button nums = (Button)v;
+            String texto = nums.getText().toString();
+            res = label.getText().toString();
+            if (label.getText().toString() == "0" && texto == "0"){
+                label.setText("0");
+            }
+            else {
+                res = res + texto;
+                label.setText(res);
+            }
         }
     };
 
